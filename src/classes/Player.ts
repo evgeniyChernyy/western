142ae1c8b91c76a6a5d4358d7fa262b7346482a9
Evgeniy Chernyy <e.chernyj@notamedia.ru>
2024-06-02 23:41:55 +0300

    added muzzle fire when shooting

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -1,9 +1,14 @@
-import { Physics, Input } from 'phaser';
+import { Physics, GameObjects, Input } from 'phaser';
 import {PLAYER_DEPTH, PLAYER_SPEED, UI_DEPTH} from "../constants"
 
 export class Player extends Physics.Matter.Sprite{
 
+    muzzleFire : GameObjects.Image
+    aim : GameObjects.Image
+
     moveKeys : object
+    canShoot : boolean
+    currentWeapon : object
 
     constructor(config) {
         super(config.scene.matter.world,config.x,config.y,"player",null,{
@@ -13,6 +18,22 @@ export class Player extends Physics.Matter.Sprite{
         })
 
         this.setScale(.5,.5).setDepth(PLAYER_DEPTH)
+
+        // config and state
+        this.canShoot = true
+        this.currentWeapon = {
+            name:"pistols",
+            shootDelay:500,
+            shakeDuration:25,
+            shakeIntensity:0.01,
+            offsetX1:50,
+            offsetY1:20,
+            offsetX2:50,
+            offsetY2:-20,
+        }
+
+        // muzzle fire
+        this.muzzleFire = config.scene.add.image(0,0,"muzzle_fire").setVisible(false).setScale(.4,.4).setOrigin(0,.5)
 
         // aim
         this.aim = config.scene.add.image(this.x + 100,this.y,"aim_cursor").setScale(.75,.75).setDepth(UI_DEPTH)
@@ -26,7 +47,6 @@ export class Player extends Physics.Matter.Sprite{
             scene.game.input.mouse.requestPointerLock();
         });
 
-        // Creates object for input with WASD kets
         this.moveKeys = scene.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
@@ -57,12 +77,51 @@ export class Player extends Physics.Matter.Sprite{
             }
         });
     }
-    update(delta){
+    shoot(pistolIndex = 1){
+        this.canShoot = false
+        // пока без отдачи+
+        // knockback(player,150,player.rotation);
+
+        this.scene.cameras.main.shake(this.currentWeapon.shakeDuration,this.currentWeapon.shakeIntensity,true);
+
+        let muzzleX = this.currentWeapon["offsetX" + pistolIndex] * Math.cos(this.rotation) - this.currentWeapon["offsetY" + pistolIndex] * Math.sin(this.rotation),
+            muzzleY = this.currentWeapon["offsetX" + pistolIndex] * Math.sin(this.rotation) + this.currentWeapon["offsetY" + pistolIndex] * Math.cos(this.rotation)
+        this.muzzleFire.setPosition(this.x + muzzleX,this.y + muzzleY)
+        this.muzzleFire.rotation = this.rotation
+        this.muzzleFire.setVisible(true)
+
+        this.scene.time.addEvent({
+            delay:this.currentWeapon.shootDelay,
+            callback:()=>{
+                this.canShoot = true
+
+                if(pistolIndex === 1){
+                    this.shoot(2)
+                }
+            },
+        })
+        this.scene.time.addEvent({
+            delay:25,
+            callback:()=>{
+                this.muzzleFire.setVisible(false)
+            },
+        })
+
+        // player.reticleSpread += 1;
+        // new Bullet(game, player.x, player.y, 'atlas', 'bullet0001', 1, player, 400, 0);
+    }
+    update(){
+        // rotation
         this.rotation = Phaser.Math.Angle.Between(
             this.x,
             this.y,
             this.aim.x,
             this.aim.y
         )
+
+        // firing
+        if(this.scene.input.activePointer.isDown && this.canShoot) {
+            this.shoot()
+        }
     }
 }
